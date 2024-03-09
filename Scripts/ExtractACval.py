496d4d46c841bc04d46c0cdb084a3bcaf6ff0040
kaftkatt <92925614+kaftkatt@users.noreply.github.com>
2024-03-09 17:52:26 +0100

    Automating ExtractACval.py for remote work

--- Scripts/ExtractACval.py
+++ Scripts/ExtractACval.py
@@ -3,95 +3,67 @@ import numpy as np
 import SVBfunc
 import xarray as xr
 # In[141]:
-var='PHIHYD'
+
 dep=55
+for var in ['phiHyd', 'rhoAnoma', 'dynVars']:
+	dirw = '/home/athelandersson/NETCDFs/smooth/'
+	dirn = '/home/athelandersson/NETCDFs/smooth_NO/'
 
-if np.logical_or(var == 'PHIHYD',var == 'RHO'):
-	pathw = '/media/amelia/Trillian/SVB/exp06_512x612x100_ORL_SVB/01b_SVB_febTS/'
-	pathn = '/media/amelia/Trillian/SVB/exp06_512x612x100_ORL/01b_noSVB_febTS/'
-	varname=var
-else:
-	pathw = '/media/amelia/Trillian/SVB/exp06_512x612x100_ORL_SVB/01_SVB_febTS/'
-	pathn = '/media/amelia/Trillian/SVB/exp06_512x612x100_ORL/01_noSVB_febTS/'
-	varname='DYNVARS'
+	dsw, dsn = SVBfunc.loadNetCDFs(dirw, dirn, varname)
 
-dsw, dsn = SVBfunc.loadNetCDFs(pathw, pathn, varname)
-
-name='Val'
-name2='lonAC'
-name3='latAC'
-
-LAT = dsw[0].YC
-LON = dsw[0].XC - 360
-
-Zdyn = dsw[0].Z
-hFacCw = dsw[0].hFacC
-hFacCn = dsn[0].hFacC
-hFacCusew=hFacCw.values
-hFacCusen=hFacCn.values
-
-
-
-pathn= str(var)+'ACdep55noSVB.nc'
-pathw= str(var)+'ACdep55withSVB.nc'
-
-
-for l in np.arange(0,8,1):
+	name='Val'
+	name2='lonAC'
+	name3='latAC'
 	
-	exec (f'W=dsw[l].{var}[:,55,:,:].values')
-	exec (f'N=dsn[l].{var}[:,55,:,:].values')
+	LAT = dsw[0].YC
+	LON = dsw[0].XC - 360
 	
-	TIMEdyn=dsw[l].time.astype(int).values*1e-9
-	ntdyn = np.size(TIMEdyn)
+	Zdyn = dsw[0].Z
+	hFacCw = dsw[0].hFacC
+	hFacCn = dsn[0].hFacC
+	hFacCusew=hFacCw.values
+	hFacCusen=hFacCn.values
 	
-	val, lon_fix,lat_fix,dist_cummul=SVBfunc.varsalongcoasts(hFacCusew,LAT,LON,Zdyn,W,ntdyn,dep,l)
-	valn,lon_fixn,lat_fixn,dist_cummuln=SVBfunc.varsalongcoasts(hFacCusen,LAT,LON,Zdyn,N,ntdyn,dep,l)
+	
+	
+	pathn= str(var)+'ACdep55noSVB.nc'
+	pathw= str(var)+'ACdep55withSVB.nc'
+
+
+	for l in np.arange(0,10,1):
+		
+		exec (f'W=dsw[l].{var}[:,55,:,:].values')
+		exec (f'N=dsn[l].{var}[:,55,:,:].values')
+		
+		TIMEdyn=dsw[l].time.astype(int).values*1e-9
+		ntdyn = np.size(TIMEdyn)
+		
+		val, lon_fix,lat_fix,dist_cummul=SVBfunc.varsalongcoasts(hFacCusew,LAT,LON,Zdyn,W,ntdyn,dep,l)
+		valn,lon_fixn,lat_fixn,dist_cummuln=SVBfunc.varsalongcoasts(hFacCusen,LAT,LON,Zdyn,N,ntdyn,dep,l)
+		    
+		if l == 0: valOUTw = val;valOUTn = valn;time = TIMEdyn
+		else: valOUTw=np.concatenate((valOUTw,val),axis=0); valOUTn=np.concatenate((valOUTn,valn),axis=0); time=np.concatenate((time,TIMEdyn),axis=0)
+		print(str(l))
+		
 	    
-	if l == 0: valOUTw = val;valOUTn = valn;time = TIMEdyn
-	else: valOUTw=np.concatenate((valOUTw,val),axis=0); valOUTn=np.concatenate((valOUTn,valn),axis=0); time=np.concatenate((time,TIMEdyn),axis=0)
-	print(str(l))
-
-    
-dsnALL = xr.Dataset({name: (("time","x"), valOUTn),
-                 name2:(("x"), lon_fixn),
-                 name3:(("x"), lat_fixn)
-                    },
-           coords ={
-                 "x" : dist_cummuln,
-                 "time": time,
-              },
-               )
-dswALL = xr.Dataset({name: (("time","x"), valOUTw),
-                 name2:(("x"), lon_fix),
-                 name3:(("x"), lat_fix)
-                    },
-           coords ={
-                 "x" : dist_cummul,
-                 "time": time,
-              },
-               )
+	dsnALL = xr.Dataset({name: (("time","x"), valOUTn),
+	                 name2:(("x"), lon_fixn),
+	                 name3:(("x"), lat_fixn)
+	                    },
+	           coords ={
+	                 "x" : dist_cummuln,
+	                 "time": time,
+	              },
+	               )
+	dswALL = xr.Dataset({name: (("time","x"), valOUTw),
+	                 name2:(("x"), lon_fix),
+	                 name3:(("x"), lat_fix)
+	                    },
+	           coords ={
+	                 "x" : dist_cummul,
+	                 "time": time,
+	              },
+	               )
                
-dsnALL.to_netcdf(pathn)
-dswALL.to_netcdf(pathw)
-
-
--------------------------------------------------------------------------------------------------
-#FILTER
-import xarray as xr
-import SVBfunc
-
-varname='PHIHYD'
-pathn= str(varname)+'ACdep55noSVB.nc'
-pathw= str(varname)+'ACdep55withSVB.nc'
-
-dswALL = xr.open_dataset(pathw)
-dsnALL = xr.open_dataset(pathn)
-
-valw=dswALL.Val
-valn=dsnALL.Val
-
-FILENAME=str(varname)+ 'AC.nc'
-
-SVBfunc.SavingFilteredValues(valw,valn,dswALL,FILENAME,1,1)
-
-
+	dsnALL.to_netcdf(pathn)
+	dswALL.to_netcdf(pathw)
