0913f9fb1fd126df50e4fff3d905df2e53858193
kaftkatt <92925614+kaftkatt@users.noreply.github.com>
2024-03-11 21:36:09 +0100

    Update FilterAlongCoast.py

--- Scripts/FilterAlongCoast.py
+++ Scripts/FilterAlongCoast.py
@@ -1,10 +1,10 @@
 import xarray as xr
 import SVBfunc
 
-for varname in ['phiHyd', 'rhoAnoma', 'dynVars']:
+for varname in ['PHIHYD', 'RHOAnoma', 'SALT','THETA','UVEL','VVEL','WVEL','ETAN]:
 
-  pathn= str(varname)+'ACdep55noSVB.nc'
-  pathw= str(varname)+'ACdep55withSVB.nc'
+  pathn= '/home/athelandersson/NETCDFs/' + str(varname)+'ACnoSVBPREFILT.nc'
+  pathw= '/home/athelandersson/NETCDFs/' + str(varname)+'ACwithSVBPREFILT.nc'
 
   dswALL = xr.open_dataset(pathw)
   dsnALL = xr.open_dataset(pathn)
