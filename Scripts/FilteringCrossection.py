711a3d3210baac2eba9edb1b62f769b9db548e09
kaftkatt <92925614+kaftkatt@users.noreply.github.com>
2024-03-20 22:19:50 +0100

    Update FilteringCrossection.py

--- Scripts/FilteringCrossection.py
+++ Scripts/FilteringCrossection.py
@@ -5,48 +5,45 @@ hej=[35,54,79,120,154,194,219]
 corrind=[30.49,30.77,31.13,31.69,32.11,32.65,33.02] #[30.49,31.69,32.11] # 30.4884, 31.6852, 32.1068
 varlist=['PHIHYD','UVEL','VVEL','WVEL']
 
-#for var in varlist:
-var='WVEL'
-for i in range(len(hej)):
-	if var=='PHIHYD':
-		dirn='/media/amelia/Trillian/SVB/exp06_512x612x100_ORL/01b_noSVB_febTS/'
-		dirw='/media/amelia/Trillian/SVB/exp06_512x612x100_ORL_SVB/01b_SVB_febTS/'
-		
-		dsw,dsn=SVBfunc.loadNetCDFs(dirw,dirn,'PHIHYD')
-	else:	
-		dirn='/media/amelia/Trillian/SVB/exp06_512x612x100_ORL/01_noSVB_febTS/'
-		dirw='/media/amelia/Trillian/SVB/exp06_512x612x100_ORL_SVB/01_SVB_febTS/'
-		
-		dsw,dsn=SVBfunc.loadNetCDFs(dirw,dirn,'DYNVARS')
-		
-	VALfilt,VALfilttwe,VALMITpre,dist,Z,times,timestwe=SVBfunc.ExtractAndFiltCrossect(i,dsw,dsn,1,1,var,corrind)
-	
-	
-	FILENAME='Locations/' + str(var) + str(corrind[i]) + 'no.nc'
-	ds = xr.Dataset({'VAL': (("time","z","x"), VALMITpre)
-			    },
-			coords ={
-			    "x" : dist,
-			    "z" : Z,
-			    "time": times
-			},
-			)
+dirn='/home/athelandersson/NETCDFs/smooth_NO/'
+dirw='/home/athelandersson/NETCDFs/smooth/'
+
+for var in varlist:
+	var='WVEL'
+	for i in range(len(hej)):
+		if var=='PHIHYD':			
+			dsw,dsn=SVBfunc.loadNetCDFs(dirw,dirn,'phiHyd')
+		else:	
+			dsw,dsn=SVBfunc.loadNetCDFs(dirw,dirn,'dynVars')
 			
-	ds.to_netcdf(FILENAME)
+		VALfilt,VALfilttwe,VALMITpre,dist,Z,times,timestwe=SVBfunc.ExtractAndFiltCrossect(i,dsw,dsn,1,1,var,corrind)
+		
+		
+		FILENAME='Locations/' + str(var) + str(corrind[i]) + 'no.nc'
+		ds = xr.Dataset({'VAL': (("time","z","x"), VALMITpre)
+				    },
+				coords ={
+				    "x" : dist,
+				    "z" : Z,
+				    "time": times
+				},
+				)
+				
+		ds.to_netcdf(FILENAME)
+		
+		
+		FILENAMEfilt='Locations/' + str(var) + str(corrind[i]) + 'filt.nc'
+		dsf = xr.Dataset({'VAL': (("time","z","x"), VALfilt),
+				'VAL2': (("time2","z","x"), VALfilttwe)
+				    },
+				coords ={
+				    "x" : dist,
+				    "z" : Z,
+				    "time": times,
+				    "time2": timestwe
+				},
+				)
+		dsf.to_netcdf(FILENAMEfilt)
 	
 	
-	FILENAMEfilt='Locations/' + str(var) + str(corrind[i]) + 'filt.nc'
-	dsf = xr.Dataset({'VAL': (("time","z","x"), VALfilt),
-			'VAL2': (("time2","z","x"), VALfilttwe)
-			    },
-			coords ={
-			    "x" : dist,
-			    "z" : Z,
-			    "time": times,
-			    "time2": timestwe
-			},
-			)
-	dsf.to_netcdf(FILENAMEfilt)
-
-
-
+	
