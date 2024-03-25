ccdf7041441cb562da30409d38dd5d14f37c80d3
athelandersson <athelandersson@aha.ucsd.edu>
2024-03-25 11:54:44 -0700

    workingfilter

--- Scripts/SVBfunc.py
+++ Scripts/SVBfunc.py
@@ -426,7 +426,7 @@ def FiltDetrend(VAL,filt,detrend,fs,fs2):
 		order = 3      
 		cutoff =np.array([1/432000, 1/43200])
 
-		if fs2 == 0:
+		if fs2 != 0:
 			inds=np.append(np.arange(0,433,2),np.arange(433,792,1))
 			VALfiltAll=np.zeros(np.shape(VALdif[inds,:]))
 
