d4c7a5db6ca69c88a76e754825cc18878fd9f392
kaftkatt <92925614+kaftkatt@users.noreply.github.com>
2024-04-09 15:58:40 +0200

    Update ExtractACval.py

--- Scripts/ExtractACval.py
+++ Scripts/ExtractACval.py
@@ -5,11 +5,20 @@ import xarray as xr
 # In[141]:
 
 dep=55
-for var in ['phiHyd', 'rhoAnoma', 'dynVars']:
-	dirw = '/home/athelandersson/NETCDFs/original/'
-	dirn = '/home/athelandersson/NETCDFs/original_NO/'
+coast='original'
+
+if coast== 'original':
+  tstart=2
+  ts=8
+elif coast == 'smooth':
+  tstart=1
+  ts=9
+  
+for var in ['phiHyd', 'rhoAnoma', 'dynVars','eta']:
+	dirw = '/home/athelandersson/NETCDFs/' +  str(coast) + '/'
+	dirn = '/home/athelandersson/NETCDFs/' +  str(coast) + '_NO/'
 	
-	dsw, dsn = SVBfunc.loadNetCDFs(dirw, dirn, var)
+	dsw, dsn = SVBfunc.loadNetCDFs(dirw, dirn, var,tstart)
 	
 	name='Val'
 	name2='lonAC'
@@ -23,12 +32,12 @@ for var in ['phiHyd', 'rhoAnoma', 'dynVars']:
 	hFacCn = dsn[0].hFacC
 	hFacCusew=hFacCw.values
 	hFacCusen=hFacCn.values
-	
+
 	if var == 'dynVars': 
 		for VAR in ['UVEL','VVEL','WVEL','SALT','THETA']:
-			pathn= '/home/athelandersson/NETCDFs/original_NO/' + str(VAR)+'ACnoSVBPREFILT.nc'
-			pathw= '/home/athelandersson/NETCDFs/original/' + str(VAR)+'ACwithSVBPREFILT.nc'
-			for l in np.arange(0,8,1):
+			pathn= '/home/athelandersson/NETCDFs/' +  str(coast) + '_NO/' + str(VAR)+ 'ACnoSVBPREFILT.nc'
+			pathw= '/home/athelandersson/NETCDFs/' +  str(coast) + '/' + str(VAR)+'ACwithSVBPREFILT.nc'
+			for l in np.arange(0,ts,1):
 				
 				exec (f'W=dsw[l].{VAR}[:,55,:,:].values')
 				exec (f'N=dsn[l].{VAR}[:,55,:,:].values')
@@ -46,35 +55,77 @@ for var in ['phiHyd', 'rhoAnoma', 'dynVars']:
 			
 			
 			dsnALL = xr.Dataset({name: (("time","x"), valOUTn),
-			                 name2:(("x"), lon_fixn),
-			                 name3:(("x"), lat_fixn)
-			                    },
-			           coords ={
-			                 "x" : dist_cummuln,
-			                 "time": time,
-			              },
-			               )
+					 name2:(("x"), lon_fixn),
+					 name3:(("x"), lat_fixn)
+					    },
+				   coords ={
+					 "x" : dist_cummuln,
+					 "time": time,
+				      },
+				       )
 			dswALL = xr.Dataset({name: (("time","x"), valOUTw),
-			                 name2:(("x"), lon_fix),
-			                 name3:(("x"), lat_fix)
-			                    },
-			           coords ={
-			                 "x" : dist_cummul,
-			                 "time": time,
-			              },
-			               )
-		        
+					 name2:(("x"), lon_fix),
+					 name3:(("x"), lat_fix)
+					    },
+				   coords ={
+					 "x" : dist_cummul,
+					 "time": time,
+				      },
+				       )
+			
 			dsnALL.to_netcdf(pathn)
 			dswALL.to_netcdf(pathw)
-	
+	elif var == 'eta': 
+		VAR='ETAN'
+		pathn= '/home/athelandersson/NETCDFs/' +  str(coast) + '_NO/' + str(VAR)+ 'ACnoSVBPREFILT.nc'
+		pathw= '/home/athelandersson/NETCDFs/' +  str(coast) + '/' + str(VAR)+'ACwithSVBPREFILT.nc'	
+		for l in np.arange(0,ts,1):
+			
+			exec (f'W=dsw[l].{VAR}[:,:,:]')
+			exec (f'N=dsn[l].{VAR}[:,:,:]')
+			
+			TIMEdyn=dsw[l].time.astype(int).values*1e-9
+			nt = np.size(TIMEdyn)
+			
+			val, lon_fix,lat_fix,dist_cummul=SVBfunc.loadingcoastpts(hFacCusew,Zdyn,LAT,LON,W,nt,l,VAR)
+			valn,lon_fixn,lat_fixn,dist_cummuln=SVBfunc.loadingcoastpts(hFacCusen,Zdyn,LAT,LON,N,nt,l,VAR)
+			    
+			if l == 0: valOUTw = val;valOUTn = valn;time = TIMEdyn
+			else: valOUTw=np.concatenate((valOUTw,val),axis=0); valOUTn=np.concatenate((valOUTn,valn),axis=0); time=np.concatenate((time,TIMEdyn),axis=0)
+			print(str(l))
+			
+		
+		dsnALL = xr.Dataset({name: (("time","x"), valOUTn),
+				 name2:(("x"), lon_fixn),
+				 name3:(("x"), lat_fixn)
+				    },
+			   coords ={
+				 "x" : dist_cummuln,
+				 "time": time,
+			      },
+			       )
+		dswALL = xr.Dataset({name: (("time","x"), valOUTw),
+				 name2:(("x"), lon_fix),
+				 name3:(("x"), lat_fix)
+				    },
+			   coords ={
+				 "x" : dist_cummul,
+				 "time": time,
+			      },
+			       )
+		
+		dsnALL.to_netcdf(pathn)
+		dswALL.to_netcdf(pathw)
+
 	else: 
 		if var == 'phiHyd':
 			VAR='PHIHYD'
 		elif var == 'rhoAnoma': 
 			VAR='RHOAnoma'
-		pathn= '/home/athelandersson/NETCDFs/original_NO/' + str(VAR)+'ACnoSVBPREFILT.nc'
-		pathw= '/home/athelandersson/NETCDFs/original/' + str(VAR)+'ACwithSVBPREFILT.nc'	
-		for l in np.arange(0,8,1):
+			
+		pathn= '/home/athelandersson/NETCDFs/' +  str(coast) + '_NO/' + str(VAR)+ 'ACnoSVBPREFILT.nc'
+		pathw= '/home/athelandersson/NETCDFs/' +  str(coast) + '/' + str(VAR)+'ACwithSVBPREFILT.nc'
+		for l in np.arange(0,ts,1):
 			
 			exec (f'W=dsw[l].{VAR}[:,55,:,:].values')
 			exec (f'N=dsn[l].{VAR}[:,55,:,:].values')
@@ -91,23 +142,24 @@ for var in ['phiHyd', 'rhoAnoma', 'dynVars']:
 			
 		
 		dsnALL = xr.Dataset({name: (("time","x"), valOUTn),
-		                 name2:(("x"), lon_fixn),
-		                 name3:(("x"), lat_fixn)
-		                    },
-		           coords ={
-		                 "x" : dist_cummuln,
-		                 "time": time,
-		              },
-		               )
+				 name2:(("x"), lon_fixn),
+				 name3:(("x"), lat_fixn)
+				    },
+			   coords ={
+				 "x" : dist_cummuln,
+				 "time": time,
+			      },
+			       )
 		dswALL = xr.Dataset({name: (("time","x"), valOUTw),
-		                 name2:(("x"), lon_fix),
-		                 name3:(("x"), lat_fix)
-		                    },
-		           coords ={
-		                 "x" : dist_cummul,
-		                 "time": time,
-		              },
-		               )
+				 name2:(("x"), lon_fix),
+				 name3:(("x"), lat_fix)
+				    },
+			   coords ={
+				 "x" : dist_cummul,
+				 "time": time,
+			      },
+			       )
 		
 		dsnALL.to_netcdf(pathn)
 		dswALL.to_netcdf(pathw)
+	
