8dfcf553cac668c05e6c1ec403f4194e8cae2cab
kaftkatt <92925614+kaftkatt@users.noreply.github.com>
2024-05-05 14:19:23 +0200

    Update FilteringCrossection.py

--- Scripts/FilteringCrossection.py
+++ Scripts/FilteringCrossection.py
@@ -1,12 +1,14 @@
 import SVBfunc
 import xarray as xr
 
+coast='original'
+
 hej=[35,54,79,120,154,194,219]  
 corrind=[30.49,30.77,31.13,31.69,32.11,32.65,33.02] #[30.49,31.69,32.11] # 30.4884, 31.6852, 32.1068
 varlist=['PHIHYD','UVEL','VVEL','WVEL']
 
-dirn='/home/athelandersson/NETCDFs/smooth_NO/'
-dirw='/home/athelandersson/NETCDFs/smooth/'
+dirn='/home/athelandersson/NETCDFs/' + str(coast) + '_NO/'
+dirw='/home/athelandersson/NETCDFs/' + str(coast) + '/'
 
 for var in varlist:
 	for i in range(len(hej)):
@@ -18,7 +20,7 @@ for var in varlist:
 		VALfilt,VALMITpre,dist,Z,times=SVBfunc.ExtractAndFiltCrossectNEW(i,dsw,dsn,1,1,var,corrind)
 		
 		
-		FILENAME='/home/athelandersson/CTW-analysis/Files/Locations/' + str(var) + str(corrind[i]) + 'no.nc'
+		FILENAME='/home/athelandersson/CTW-analysis/Files/' + str(coast) + '/Locations/' + str(var) + str(corrind[i]) + 'no.nc'
 		ds = xr.Dataset({'VAL': (("time","z","x"), VALMITpre)
 				    },
 				coords ={
@@ -31,7 +33,7 @@ for var in varlist:
 		ds.to_netcdf(FILENAME)
 		
 		
-		FILENAMEfilt='/home/athelandersson/CTW-analysis/Files/Locations/' + str(var) + str(corrind[i]) + 'filt.nc'
+		FILENAMEfilt='/home/athelandersson/CTW-analysis/Files/' + str(coast) + '/Locations/' + str(var) + str(corrind[i]) + 'filt.nc'
 		dsf = xr.Dataset({'VAL': (("time","z","x"), VALfilt)
 				    },
 				coords ={
