8e2c6e0ed4872400afdc892fb5cbe1bf11295bd1
athelandersson <athelandersson@aha.ucsd.edu>
2024-06-28 11:44:22 -0700

    updating the whole folder

--- ScriptsN/CreateNETCDFs.py
+++
@@ -1,6 +0,0 @@
-from SVBfunc import createNetCDF
-
-#for coast in ['smooth', 'straight']:
-coast='originial'
-for prefix in ['phiHyd', 'rhoAnoma', 'eta', 'dynVars']:
-        createNetCDF(coast,prefix, prefix)
