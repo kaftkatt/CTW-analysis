cab303f2ccea5ca977942d294817242b000af06d
athelandersson <athelandersson@aha.ucsd.edu>
2024-09-06 10:02:18 -0700

    update

--- Scripts/RunLinReg.py
+++ Scripts/RunLinReg.py
@@ -1,10 +1,12 @@
 import SVBfunc
 VAR=['ashore','cshore'] #['PHIHYD','VVEL','UVEL','WVEL']
 FILT=['no','filt']
-COAST=['smooth','orginal']
-for coast in COAST:
-	for var in VAR:
-		for filt in FILT:
-			SVBfunc.linearregressionSave(filt,var,coast)
-			print('Done with ' + str(coast) + ' ' + str(var) + ' ' +  str(filt))
+#COAST=['smooth','original']
+#for coast in COAST:
+
+coast='original'
+for var in VAR:
+	for filt in FILT:
+		SVBfunc.linearregressionSave(filt,var,coast)
+		print('Done with ' + str(coast) + ' ' + str(var) + ' ' +  str(filt))
 
