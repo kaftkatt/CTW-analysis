6d37514ab7d82e316edd0a9d50f6bac39b15c42b
athelandersson <athelandersson@aha.ucsd.edu>
2024-10-23 16:20:25 -0700

    newplot

--- Scripts/ExtractIndicesPerp2coast.py
+++ Scripts/ExtractIndicesPerp2coast.py
@@ -250,7 +250,7 @@ ax = fig.add_subplot(gs[1, 0])
 vmin=-5
 vmax=5
 cbarall=0
-SVBfunc.plot_HOVMOLLER(ax,distVEL,TIMEVEL,WVEL*1e6,'','Vertical velocity  [$10^{-6}$ ms$^{-1}$]',vmin,vmax,fig,lat_acVEL,lon_acVEL,1,cbarall,'(c)')
+SVBfunc.plot_HOVMOLLER(ax,distVEL,TIMEVEL,WVEL*1e6,'','Vertical velocity  [$10^{-6}$ ms$^{-1}$]',vmin,vmax,fig,lat_acVEL,lon_acVEL,0,cbarall,'(c)')
 
 for i in range(len(hej2ind)):
 	if len(hej2ind)>20:
