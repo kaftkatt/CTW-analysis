27fbca0b3523b240deb373206dc65144d271700a
athelandersson <athelandersson@aha.ucsd.edu>
2024-10-26 23:10:21 -0700

    new TYpe of figure

--- Scripts/Figure 2 WAVES .py
+++ Scripts/Figure 2 WAVES .py
@@ -8,12 +8,12 @@ import pylab as pl
 
 from SVBfunc import haversine
 
-coast = 'smooth'
+coast = 'original'
 
 i=0
 varname='dynVars'
-pathn='/home/athelandersson/NETCDFs/'+ str(coast) + '/'+ str(varname)+'noSVB'+ str(2+i)+'_'+ str(3+i) +'.nc'
-pathw='/home/athelandersson/NETCDFs/'+ str(coast) +'_NO/'+ str(varname)+'withSVB'+ str(2+i)+'_'+ str(3+i) +'.nc'
+pathn='/home/athelandersson/NETCDFs/'+ str(coast) + '_NO/'+ str(varname)+'noSVB'+ str(2+i)+'_'+ str(3+i) +'.nc'
+pathw='/home/athelandersson/NETCDFs/'+ str(coast) +'/'+ str(varname)+'withSVB'+ str(2+i)+'_'+ str(3+i) +'.nc'
         
 dsw  = xr.open_dataset(pathw)
 dsn = xr.open_dataset(pathn)
@@ -26,8 +26,8 @@ TIMEvel=time*1e-9
 
 i2=2
 varname='dynVars'
-pathn2='/home/athelandersson/NETCDFs/'+ str(coast) + '/'+ str(varname)+'noSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
-pathw2='/home/athelandersson/NETCDFs/'+ str(coast) +'_NO/'+ str(varname)+'withSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
+pathn2='/home/athelandersson/NETCDFs/'+ str(coast) + '_NO/'+ str(varname)+'noSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
+pathw2='/home/athelandersson/NETCDFs/'+ str(coast) +'/'+ str(varname)+'withSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
         
 dsw2  = xr.open_dataset(pathw2)
 dsn2 = xr.open_dataset(pathn2)
@@ -39,8 +39,8 @@ TIMEvel2=time2*1e-9
 
 i2=6
 varname='dynVars'
-pathn3='/home/athelandersson/NETCDFs/'+ str(coast) + '/'+ str(varname)+'noSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
-pathw3='/home/athelandersson/NETCDFs/'+ str(coast) + '_NO/'+ str(varname)+'withSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
+pathn3='/home/athelandersson/NETCDFs/'+ str(coast) + '_NO/'+ str(varname)+'noSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
+pathw3='/home/athelandersson/NETCDFs/'+ str(coast) + '/'+ str(varname)+'withSVB'+ str(2+i2)+'_'+ str(3+i2) +'.nc'
         
 dsw3  = xr.open_dataset(pathw3)
 dsn3 = xr.open_dataset(pathn3)
@@ -51,10 +51,10 @@ time3=dsw3.time.values.astype(int)
 TIMEvel3=time3*1e-9
 
 
-pathETA='/media/amelia/Trillian/SVB/ETA.nc'
+pathETA='/home/athelandersson/NETCDFs/' + str(coast) + '/ETA.nc'
 dsETA= xr.open_dataset(pathETA)
 
-etafiltall=dsETA.ETAfiltall.values
+etafiltall=dsETA.VALfilt.values
 LON=dsETA.x.values
 LAT=dsETA.y.values
 TIME=dsETA.time.values
@@ -73,7 +73,7 @@ plt.rcParams['figure.dpi'] = 300
 plt.rcParams['savefig.dpi'] = 300
 
 fig = plt.figure()
-gs = GridSpec(nrows=3, ncols=2,hspace=0.01)
+gs = GridSpec(nrows=1, ncols=2,hspace=0.01)
 
 vmin=-0.009*10
 vmax=0.009*10
@@ -86,7 +86,7 @@ xlab='Longitude [°]'
 ylab='Latitude [°]'
 
 ax = fig.add_subplot(gs[0,0]) 
-
+'''
 ax.set_facecolor('tan')
 cax = ax.pcolormesh(LON,LAT,np.ma.masked_array(etafiltall[ind,:,:]*1000, mask=mask[0,:,:]),cmap=cmocean.cm.curl,vmin=vmin,vmax=vmax)
 ax.contour(LON,LAT,depth,  colors=['0.2','0.6'], 
@@ -110,11 +110,18 @@ ax.set_ylim(27,35.3)
 ax.set_aspect(1)
 
 ax = fig.add_subplot(gs[1,0]) 
-
+'''
 ax.set_facecolor('tan')
 cax = ax.pcolormesh(LON,LAT,np.ma.masked_array(etafiltall[ind2,:,:]*1000, mask=mask[0,:,:]),cmap=cmocean.cm.curl,vmin=vmin,vmax=vmax)
 ax.contour(LON,LAT,depth,  colors=['0.2','0.6'], 
                 levels=[0,500])
+
+divider = make_axes_locatable(ax)
+axdiv = divider.new_vertical(size = '5%', pad = 0.5)
+fig.add_axes(axdiv)
+cbar_ax = plt.colorbar(cax, cax=axdiv,orientation='horizontal')
+cbar_ax.ax.xaxis.set_label_position("top")
+cbar_ax.set_label('SSH [mm]')
 
 ax.tick_params(axis='x',which='both', bottom=True, top=False, labelbottom=False)
 ax.set(ylabel=ylab)
@@ -126,7 +133,7 @@ ax.text(0.4,0.87, f'Surface \nDay {TIME[ind2]/(60*24)}', transform=ax.transAxes,
 ax.set_xlim(-122,-114) 
 ax.set_ylim(27,35.3)
 ax.set_aspect(1)
-
+'''
 ax = fig.add_subplot(gs[2,0]) 
 
 ax.set_facecolor('tan')
@@ -144,14 +151,14 @@ ax.text(0.4,0.87, f'Surface \nDay {TIME[ind3]/(60*24)}', transform=ax.transAxes,
 ax.set_xlim(-122,-114) 
 ax.set_ylim(27,35.3)
 ax.set_aspect(1)
-
+'''
 
 vmin=-0.000002*1e6
 vmax=0.000002*1e6
 
 
 ax = fig.add_subplot(gs[0,1]) 
-
+'''
 ax.set_facecolor('tan')
 cax = ax.pcolormesh(LON,LAT,np.ma.masked_array(Ww-Wn, mask=mask[55,:,:])*1e6,cmap=cmocean.cm.balance,vmin=vmin,vmax=vmax)
 ax.contour(LON,LAT,depth, colors=['0.2','0.6'], 
@@ -175,12 +182,17 @@ ax.set_ylim(27,35.3)
 ax.set_aspect(1)
 
 ax = fig.add_subplot(gs[1,1]) 
-
+'''
 ax.set_facecolor('tan')
 cax = ax.pcolormesh(LON,LAT,np.ma.masked_array(Ww2-Wn2, mask=mask[55,:,:])*1e6,cmap=cmocean.cm.balance,vmin=vmin,vmax=vmax)
 ax.contour(LON,LAT,depth, colors=['0.2','0.6'], 
                 levels=[0,500])
-
+divider = make_axes_locatable(ax)
+axdiv = divider.new_vertical(size = '5%', pad = 0.5)
+fig.add_axes(axdiv)
+cbar_ax = plt.colorbar(cax, cax=axdiv,orientation='horizontal')
+cbar_ax.ax.xaxis.set_label_position("top")
+cbar_ax.set_label('Vertical velocity [$10^{-6}$ ms$^{-1}$]')
 ax.tick_params(axis='y',which='both', left=True, right=False, labelleft=False) 
 ax.tick_params(axis='x',which='both', bottom=True, top=False, labelbottom=False)
 
@@ -191,7 +203,7 @@ ax.text(-0.1,1.05, '(d)', transform=ax.transAxes)
 ax.set_xlim(-122,-114) 
 ax.set_ylim(27,35.3)
 ax.set_aspect(1)
-
+'''
 ax = fig.add_subplot(gs[2,1]) 
 
 ax.set_facecolor('tan')
@@ -209,5 +221,5 @@ ax.text(-0.1,1.05, '(f)', transform=ax.transAxes)
 ax.set_xlim(-122,-114) 
 ax.set_ylim(27,35.3)
 ax.set_aspect(1)
-
-plt.savefig('/home/athelandersson/CTW-analysis/Figures/' + str(coast) + 'fig2Waves.png')
+'''
+plt.savefig('/home/athelandersson/CTW-analysis/Figures/' + str(coast) + 'fig2Waves.png', transparent=True,bbox_inches='tight')
